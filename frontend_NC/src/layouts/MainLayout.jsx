741e9ea4f99d4927978ad68107574b5b0fc72821
neelngawande <neelngawande@gmail.com>
2026-05-28 13:53:52 +0530

    made some frontend improvements

--- frontend_NC/src/layouts/MainLayout.jsx
+++ frontend_NC/src/layouts/MainLayout.jsx
@@ -1,17 +1,61 @@
-import { Link, Outlet } from "react-router-dom"
+import { NavLink, Outlet } from "react-router-dom"
+import logo from "../assets/wallet-dark.svg"
 
 export default function MainLayout(){
     return(
+        <>
         <div className="min-h-screen bg-zinc-950 text-white">
-            <nav className="border-b border-zinc-800 px-6 py-4 flex gap-6">
-                <Link to="/">Dashboard</Link>
-                <Link to="/explorer">Explorer</Link>
-                <Link to="/send">Send</Link>
+
+            <nav className="border-b bg-amber-950 border-zinc-800 px-6 py-4 flex gap-6">
+
+                <img src={logo} alt="logo" className="w-10 h-10"/>
+
+                <h1 className="text-4xl font-mono font-bold pr-7 pl-2">
+                    NoobChain
+                </h1>
+
+                <NavLink
+                    to="/"
+                    className={({isActive})=>
+                        `pt-2 transition ${
+                            isActive
+                            ? "text-pink-300 text-2xl"
+                            : "hover:text-pink-300 hover:underline"
+                        }`
+                    }>
+                    Dashboard
+                </NavLink>
+
+                <NavLink
+                    to="/explorer"
+                    className={({isActive})=>
+                        `pt-2 transition ${
+                            isActive
+                            ? "text-pink-300 text-2xl"
+                            : "hover:text-pink-300 hover:underline"
+                        }`
+                    }>
+                    Explorer
+                </NavLink>
+
+                <NavLink
+                    to="/send"
+                    className={({isActive})=>
+                        `pt-2 transition ${
+                            isActive
+                            ? "text-pink-300 text-2xl"
+                            : "hover:text-pink-300 hover:underline"
+                        }`
+                    }>
+                    Send
+                </NavLink>
+
             </nav>
 
-            <main className="p-6">
+            <main className="p-5">
                 <Outlet/>
             </main>
         </div>
+        </>
     )
 }
